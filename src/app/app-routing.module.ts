ece0ace25c9ba4ce5470876af97f205f1e7890b8
zied miladi <zied.elmiladi@gmail.com>
2020-04-09 11:36:24 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {LoginComponent} from './component/login/login.component';
+import {HomeComponent} from './component/home/home.component';
+import {ContactUsComponent} from './component/contact-us/contact-us.component';
+import {AboutComponent} from './component/about/about.component';
+import {ChatComponent} from './component/chat/chat.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LoginComponent
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'contact-us', component: ContactUsComponent
+  },
+  {
+    path: 'about', component: AboutComponent
+  },
+  {
+    path: 'chats', component: ChatComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { HomeComponent } from './component/home/home.component';
+import { ContactUsComponent } from './component/contact-us/contact-us.component';
+import { AboutComponent } from './component/about/about.component';
+import { ChatComponent } from './component/chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ContactUsComponent,
+    AboutComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
